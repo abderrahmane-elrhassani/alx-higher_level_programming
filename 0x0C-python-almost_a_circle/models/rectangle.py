800c01b61a0672657f98ed26580045546f8582fd
abderrahmane-elrhassani <abdoghessan@example.com>
2024-02-12 16:49:01 -0800

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,43 +110,51 @@ class Rectangle(Base):
             **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
-    a = 0
-    while a < len(args):
-        arg = args[a]
-        if a == 0:
-            if arg is None:
-                self.__init__(self.width, self.height, self.x, self.y)
-            else:
-                self.id = arg
-        elif a == 1:
-            self.width = arg
-        elif a == 2:
-            self.height = arg
-        elif a == 3:
-            self.x = arg
-        elif a == 4:
-            self.y = arg
-        a += 1
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
 
-elif kwargs and len(kwargs) != 0:
-    keys = list(kwargs.keys())
-    values = list(kwargs.values())
-    a = 0
-    while a < len(keys):
-        k = keys[a]
-        v = values[a]
-        if k == "id":
-            if v is None:
-                self.__init__(self.width, self.height, self.x, self.y)
-            else:
-                self.id = v
-        elif k == "width":
-            self.width = v
-        elif k == "height":
-            self.height = v
-        elif k == "x":
-            self.x = v
-        elif k == "y":
-            self.y = v
-        a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
