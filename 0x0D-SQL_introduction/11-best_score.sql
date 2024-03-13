f039b1702eb2937e3ad7269e80cccbb6af3664ec
abderrahmane-elrhassani <abdoghessan@example.com>
2024-03-12 18:55:22 -0700

    11. Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Write a script that retrieves all records with a score greater than or eq__ ual to 10 from the "second_table" in the "hbtn_0c_0" database on your MyS__ QL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
