cda16bb8b029945d79a48c9692f5bf45e44ab893
abderrahmane-elrhassani <abdoghessan@example.com>
2024-03-12 18:41:34 -0700

    7. First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Write a script that inserts a new row into the "first_table" within the "__ hbtn_0c_0" database on your MySQL server.
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
