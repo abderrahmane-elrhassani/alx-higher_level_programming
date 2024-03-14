df289b12713a9058c3369625f1a47402cfaef9cd
abderrahmane-elrhassani <abdoghessan@example.com>
2024-03-13 19:40:57 -0700

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Retrieve a list of all cities within the hbtn_0d_usa database.
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id
