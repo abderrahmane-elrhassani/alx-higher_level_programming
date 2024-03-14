04a160ada70aa69782014de40c20741dad9fdace
abderrahmane-elrhassani <abdoghessan@example.com>
2024-03-13 19:13:11 -0700

    0. My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Retrieve and display the privileges granted to MySQL users user_0d_1 and user_0d_2 on the localhost server.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
